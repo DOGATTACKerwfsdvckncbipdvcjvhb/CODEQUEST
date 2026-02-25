2ed77799e7ba47e323898a10717dc7ae9c273987
KavinPratap <kavin.pratap@gmail.com>
2026-02-25 16:35:03 +0000

    s
    Co-authored-by: JaydenAngulo <JaydenAngulo@users.noreply.github.com>
    Co-authored-by: Aarush544 <Aarush544@users.noreply.github.com>

---
+++ CaeserCipher.py
@@ -0,0 +1,23 @@
+import sys
+import math
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+testCases = int(sys.stdin.readline().rstrip())
+
+answer = ""
+
+for i in range(testCases):
+    shift = int(sys.stdin.readline().rstrip())
+    scrambled = sys.stdin.readline().rstrip()
+    message = ""
+    for a in scrambled:
+        if a == ' ':
+            message += a
+        else:
+            position = alphabet.index(a)
+            message += alphabet[position - shift]
+
+    answer += message + '\n'
+
+print(answer.rstrip())
