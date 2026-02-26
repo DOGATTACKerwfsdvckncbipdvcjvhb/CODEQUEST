d382e3612b51e4a337953b033f0d491ff055cf42
KavinPratap <kavin.pratap@gmail.com>
2026-02-26 22:46:24 +0000

    gfg

--- catalogIt.py
+++ catalogIt.py
@@ -1,23 +1,4 @@
 import sys
-import math
-
-class Family:
-    def __init__(self, parent, level):
-        self.parent = parent
-        self.level = int(level)
-
-
-#
-#
-#
-
-#
-
-
-# def findChildren(objects, targetParent):
-    
-
-
 
 
 products = {}
@@ -29,56 +10,30 @@ testcases = int(sys.stdin.readline().rstrip())
 
 for i in range(testcases):
     line = (sys.stdin.readline().rstrip()).split(",")
-    products[line[0]] = Family(line[1],int(0))
+    products[line[0]] = line[1]
     
-productNames = sorted(products.keys()) ## sort wont work because it gets A2100 from sort as it sorts everything childs included.
+productNames = sorted(products.keys()) 
 
-
-#stop wait run one of the coman
-
-
-for i in products:
-    level = 4
-    temp = products[i]
-    count = 4
-    UpCount = 1
-    while count < 0:
-        
-        if temp == 'None':
-            break
-        count-=1
-        UpCount+=1
-        temp = temp.parent
-    level = UpCount
-    products[i].level = level
-print(productNames)
-
-
-
-def findChildren(target, sublist):
-    count = 1   
-    for i in sublist:
-        if products[i].parent == target:
-            answer += f"{'-' * count}{i}" 
-            count += 1
-            last_point = target
-            findChildren(i)
-    findChildren(last_point)
+target = ""
+count = 1
+sublist = productNames
 
 
 
 
 for product in productNames:
-    if products[product].parent == 'None':
-        answer += product + '\n'
-        findChildren(product, productNames)
+    if products[product] == 'None':
+        target = product
+        break
+
+def printHierarchy(target, depth):
+    global answer
+    answer += f"{'-' * depth}{target}\n"
+    for b in sublist:
+        if products[b] == target:
+            printHierarchy(b, depth + 1)
 
 
-#
-# 
+printHierarchy(target, 0)
 
-
-
-
-    
-    
+print(answer.rstrip()) 
