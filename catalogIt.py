1451cb22587d23dea9ccf902f0d32ba57d00e198
KavinPratap <kavin.pratap@gmail.com>
2026-02-26 22:01:51 +0000

    Co-authored-by: Aarush544 <Aarush544@users.noreply.github.com>

--- catalogIt.py
+++ catalogIt.py
@@ -38,32 +38,41 @@ productNames = sorted(products.keys()) ## sort wont work because it gets A2100 f
 
 
 
-for i in products:
-    level = 4
-    temp = products[i]
-    count = 4
-    UpCount = 1
-    while count < 0:
+# for i in products:
+#     level = 4
+#     temp = products[i]
+#     print(temp.parent)
+#     count = 4
+#     UpCount = 1
+#     while count > 0:
         
-        if temp == 'None':
-            break
-        count-=1
-        UpCount+=1
-        temp = temp.parent
-    level = UpCount
-    products[i].level = level
-print(productNames)
+#         if temp == 'None':
+#             break
+#         count-=1
+#         UpCount+=1
+        
+#         #temp = temp.parent
+
+#             for s in products:
+
+#     level = UpCount
+    
+#     products[i].level = level
 
 
+
+count = 1
 
 def findChildren(target, sublist):
-    count = 1   
+       
     for i in sublist:
         if products[i].parent == target:
             answer += f"{'-' * count}{i}" 
             count += 1
             last_point = target
+            sublist.remove(i)
             findChildren(i)
+    count = 1 
     findChildren(last_point)
 
 
