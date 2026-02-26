b859bee7c259e5198f3b838d4324f7efa32d4b04
KavinPratap <kavin.pratap@gmail.com>
2026-02-26 20:12:32 +0000

    s Co-authored-by: Aarush544 <Aarush544@users.noreply.github.com>

--- catalogIt.py
+++ catalogIt.py
@@ -2,27 +2,81 @@ import sys
 import math
 
 class Family:
-    def __init__(self, children, parent):
+    def __init__(self, children, parent, level):
         self.parent = parent
         self.children = children
+        self.level = int(level)
+
+
+#
+#
+#
+
+#
+
+
+# def findChildren(objects, targetParent):
+    
+
+
+
 
 products = {}
 
-answers = ''
+answers = ""
 
 testcases = int(sys.stdin.readline().rstrip())
 
 
 for i in range(testcases):
     line = (sys.stdin.readline().rstrip()).split(",")
-    products[line[0]] = Family([], line[1])
+    products[line[0]] = Family([], line[1],int(0))
     
 productNames = sorted(products.keys()) ## sort wont work because it gets A2100 from sort as it sorts everything childs included.
 
 
 
-for thing in productNames:
-    print(productNames)
+
+
+for i in products:
+    level = 4
+    temp = products[i]
+    count = 4
+    UpCount = 1
+    while count < 0:
+        
+        if temp == 'None':
+            break
+        count-=1
+        UpCount+=1
+        temp = temp.parent
+    level = UpCount
+    products[i].level = level
+print(productNames)
+
+
+
+def findChildren(target, sublist):
+    count = 1   
+    for i in sublist:
+        if products[i].parent == target:
+            answer += f"{'-' * count}{i}" 
+            count += 1
+            last_point = target
+            findChildren(i)
+    findChildren(last_point)
+
+
+
+
+# for i in productNames:
+#     if i.parent == 'None':
+#         answer += i + '\n'
+#         break 
+
+
+#
+# 
 
 
 
