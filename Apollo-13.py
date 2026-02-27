b6bee8f2cc22d1246f37e84b4f3362ff396c19da
KavinPratap <kavin.pratap@gmail.com>
2026-02-27 18:27:23 +0000

    I fix apollo 13 later

--- Apollo-13.py
+++ Apollo-13.py
@@ -3,6 +3,6 @@ import sys
 testcases = int(sys.stdin.readline().rstrip())
 answer = ""
 for i in range(testcases):
-    x, y, z = map(float, sys.stdin.readline().rsplit().split(" "))
+    x, y, z = map(float, sys.stdin.readline().rstrip().split(" "))
     answer += f"{x-180}, {y-180}, {z-180}\n"
 print(answer)
