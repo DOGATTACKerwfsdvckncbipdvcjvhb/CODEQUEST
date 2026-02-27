6feed47ca6aa9bd1c9878a52e7663f820cd33baf
KavinPratap <kavin.pratap@gmail.com>
2026-02-27 18:26:00 +0000

    d

---
+++ Apollo-13.py
@@ -0,0 +1,8 @@
+import sys
+
+testcases = int(sys.stdin.readline().rstrip())
+answer = ""
+for i in range(testcases):
+    x, y, z = map(float, sys.stdin.readline().rsplit().split(" "))
+    answer += f"{x-180}, {y-180}, {z-180}\n"
+print(answer)
