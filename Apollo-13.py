f6e7e533b20606d8990c4edf95c3121e58ffb70f
KavinPratap <kavin.pratap@gmail.com>
2026-02-27 20:16:13 +0000

    Apollo-13 finished

--- Apollo-13.py
+++ Apollo-13.py
@@ -4,5 +4,18 @@ testcases = int(sys.stdin.readline().rstrip())
 answer = ""
 for i in range(testcases):
     x, y, z = map(float, sys.stdin.readline().rstrip().split(" "))
-    answer += f"{x-180}, {y-180}, {z-180}\n"
+
+    if x < 180:
+        answer += f"{round(x+180.00,2)}"
+    else:
+        answer += f"{round(x-180.00,2)}"
+    if y < 180:
+        answer += f" {round(y+180.00,2)}"
+    else:
+        answer += f" {round(y-180.00,2)}"
+    if z < 180:
+        answer += f" {round(z+180.00,2)}\n"
+    else:
+        answer += f" {round(z-180.00,2)}\n"
+    
 print(answer)
